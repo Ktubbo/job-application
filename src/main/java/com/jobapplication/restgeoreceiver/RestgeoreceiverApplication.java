de29dc191ad25425367c03104cc67ac943e5fd26
Hubert Rusinek <rusinekh@gmail.com>
2021-12-23 11:22:29 +0100

    Initial commit

---
+++ src/main/java/com/jobapplication/restgeoreceiver/RestgeoreceiverApplication.java
@@ -0,0 +1,13 @@
+package com.jobapplication.restgeoreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestgeoreceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestgeoreceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobapplication/restgeoreceiver/RestgeoreceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobapplication.restgeoreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestgeoreceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
